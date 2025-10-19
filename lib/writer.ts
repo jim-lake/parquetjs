982a3de0791de7a05eab88154965e1840251258a
jlake <jim@blueskylabs.com>
2025-10-19 08:41:35 -0700

    we don't need the buffer convert, it works anyway

--- lib/writer.ts
+++ lib/writer.ts
@@ -510,9 +510,6 @@ async function encodePages(
     if (field.statistics !== false) {
       statistics = {};
       for (let v of distinct_values) {
-        if (field.originalType === 'UTF8' && typeof v === 'string') {
-          v = Buffer.from(v, 'utf8');
-        }
         if (statistics.min_value === undefined) {
           statistics.max_value = v as Buffer;
           statistics.min_value = v as Buffer;
